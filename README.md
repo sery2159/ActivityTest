61ed54849e6f2dd8c6aa339e0d7e1811e7a2eb0d
sery2159 <tjmi12cin6297@126.com>
2021-04-27 00:27:07 +0800

    ActivityTest

---
+++ README.md
@@ -0,0 +1,3 @@
+# ActivityTest
+
+ActivityTest
---
+++ app/src/main/java/com/lpc/activitytest/MainActivity.java
@@ -0,0 +1,70 @@
+package com.lpc.activitytest;
+
+import android.content.res.Configuration;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText et;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        log("onCreate");
+
+        et = findViewById(R.id.et);
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        log("onConfigurationChanged");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        log("onSaveInstanceState");
+        outState.putString("text", et.getText().toString());
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        log("onRestoreInstanceState");
+        et.setText(savedInstanceState.getString("text"));
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        log("onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        log("onResume");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        log("onStop");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        log("onPause");
+    }
+
+    private void log(String s){
+        Log.e("lipc0113", s);
+    }
+}
